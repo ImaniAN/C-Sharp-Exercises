91c517fc95761fc20b4b119e63e00b39a78b62f9
Imani 'K!ΠG' Niyigena <imanix8@gmail.com>
2023-06-11 15:08:51 +0200

    Console Apps + WorkSpace + First Exercise

---
+++ ConsoleApps/Exercise1.cs
@@ -0,0 +1,14 @@
+﻿// Write a C# Sharp program to print Hello and your name in a separate line.
+
+
+namespace Exercises
+{
+public class Exercise1
+{
+    public static void Main( )
+    {
+         System.Console.WriteLine("Hello World");
+         System.Console.WriteLine("Imani Niyigena!");
+    }
+}
+}
